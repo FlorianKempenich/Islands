8d8e822525064ea6016f659c63c3496f50102dc5
Florian Kempenich <Flori@nKempenich.com>
2020-01-18 17:46:36 +0000

    Implement Guess mechanism on Board

--- islands_engine/lib/islands_engine/board.ex
+++ islands_engine/lib/islands_engine/board.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Board do
-  alias IslandsEngine.Island
+  alias IslandsEngine.{Island, Coordinate}
 
   def new(), do: %{}
 
@@ -13,6 +13,42 @@ defmodule IslandsEngine.Board do
 
   def all_islands_positioned?(board) do
     Enum.all?(Island.types(), &Map.has_key?(board, &1))
+  end
+
+  def guess(board, %Coordinate{} = guess) do
+    board
+    |> try_to_hit_island(guess)
+    |> update_board_and_compute_response(board)
+  end
+
+  defp try_to_hit_island(board, guess) do
+    Enum.find_value(board, :no_island_hit, fn {island_type, island} ->
+      case Island.guess(island, guess) do
+        {:hit, island} ->
+          {island_type, island}
+
+        :miss ->
+          false
+      end
+    end)
+  end
+
+  defp update_board_and_compute_response(guess_result, board)
+
+  defp update_board_and_compute_response(:no_island_hit, board),
+    do: {:miss, :none, :no_win, board}
+
+  defp update_board_and_compute_response({island_hit_type, island_hit}, board) do
+    board = %{board | island_hit_type => island_hit}
+    {:hit, island_hit_type, win_status(board), board}
+  end
+
+  defp win_status(board) do
+    if board |> all_islands() |> Enum.all?(&Island.forested?/1) do
+      :win
+    else
+      :no_win
+    end
   end
 
   defp overlaps_with_existing_islands?(board, key, island_to_position) do
--- islands_engine/test/islands_engine/board_test.exs
+++ islands_engine/test/islands_engine/board_test.exs
@@ -2,38 +2,30 @@ defmodule IslandsEngine.BoardTest do
   use ExUnit.Case
   alias IslandsEngine.{Coordinate, Board, Island}
 
-  setup do
-    l_shape_at_2_4 = island(:l_shape, 4, 2)
-    s_shape_at_4_3 = island(:s_shape, 4, 3)
+  describe "Position island" do
+    setup :partially_complete_board
 
-    board =
-      Board.new()
-      |> Map.put(:l_shape, l_shape_at_2_4)
-      |> Map.put(:s_shape, s_shape_at_4_3)
+    test "Position non-overlapping island", %{board: board} do
+      square_at_2_2 = island(:square, 2, 2)
+      board = Board.position_island(board, :square, square_at_2_2)
+      assert Map.get(board, :square) == square_at_2_2
+    end
 
-    [board: board]
-  end
+    test "Position overlapping island", %{board: board} do
+      square_at_3_4 = island(:square, 3, 4)
+      assert {:error, :overlapping_island} = Board.position_island(board, :square, square_at_3_4)
+    end
 
-  test "Position non-overlapping island", %{board: board} do
-    square_at_2_2 = island(:square, 2, 2)
-    board = Board.position_island(board, :square, square_at_2_2)
-    assert Map.get(board, :square) == square_at_2_2
-  end
+    test "Override existing island", %{board: board} do
+      s_shape_at_4_3_hit_at_4_4 = %Island{
+        island(:s_shape, 4, 3)
+        | hit_coordinates: [%Coordinate{col: 4, row: 4}]
+      }
 
-  test "Position overlapping island", %{board: board} do
-    square_at_3_4 = island(:square, 3, 4)
-    assert {:error, :overlapping_island} = Board.position_island(board, :square, square_at_3_4)
-  end
-
-  test "Override existing island", %{board: board} do
-    s_shape_at_4_3_hit_at_4_4 = %Island{
-      island(:s_shape, 4, 3)
-      | hit_coordinates: [%Coordinate{col: 4, row: 4}]
-    }
-
-    assert Enum.empty?(board |> Map.get(:s_shape) |> Map.get(:hit_coordinates))
-    board = Board.position_island(board, :s_shape, s_shape_at_4_3_hit_at_4_4)
-    refute Enum.empty?(board |> Map.get(:s_shape) |> Map.get(:hit_coordinates))
+      assert Enum.empty?(board |> Map.get(:s_shape) |> Map.get(:hit_coordinates))
+      board = Board.position_island(board, :s_shape, s_shape_at_4_3_hit_at_4_4)
+      refute Enum.empty?(board |> Map.get(:s_shape) |> Map.get(:hit_coordinates))
+    end
   end
 
   test "All islands positioned ?" do
@@ -48,6 +40,74 @@ defmodule IslandsEngine.BoardTest do
     refute Board.all_islands_positioned?(board)
     board = Board.position_island(board, :square, island(:square, 5, 8))
     assert Board.all_islands_positioned?(board)
+  end
+
+  describe "Guess" do
+    setup :complete_board
+
+    test "Unsuccessful guess", %{board: board} do
+      {:ok, unsuccessful_guess} = Coordinate.new(9, 9)
+      assert {:miss, :none, :no_win, board} = Board.guess(board, unsuccessful_guess)
+    end
+
+    test "Successful guess, game not yet won", %{board: board} do
+      # This guess will hit the :square island
+      {:ok, successful_guess} = Coordinate.new(5, 8)
+      assert {:hit, :square, :no_win, board_after_guess} = Board.guess(board, successful_guess)
+
+      assert board_after_guess
+             |> Map.get(:square)
+             |> Map.get(:hit_coordinates)
+             |> MapSet.member?(successful_guess)
+    end
+
+    test "Successful guess, game won", %{board: board} do
+      {:hit, :atoll, :no_win, board} = Board.guess(board, %Coordinate{col: 3, row: 1})
+      {:hit, :atoll, :no_win, board} = Board.guess(board, %Coordinate{col: 3, row: 3})
+      {:hit, :atoll, :no_win, board} = Board.guess(board, %Coordinate{col: 4, row: 1})
+      {:hit, :atoll, :no_win, board} = Board.guess(board, %Coordinate{col: 4, row: 2})
+      {:hit, :atoll, :no_win, board} = Board.guess(board, %Coordinate{col: 4, row: 3})
+
+      {:hit, :dot, :no_win, board} = Board.guess(board, %Coordinate{col: 7, row: 2})
+
+      {:hit, :l_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 1, row: 1})
+      {:hit, :l_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 1, row: 2})
+      {:hit, :l_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 1, row: 3})
+      {:hit, :l_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 2, row: 3})
+
+      {:hit, :s_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 5, row: 2})
+      {:hit, :s_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 6, row: 1})
+      {:hit, :s_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 6, row: 2})
+      {:hit, :s_shape, :no_win, board} = Board.guess(board, %Coordinate{col: 7, row: 1})
+
+      {:hit, :square, :no_win, board} = Board.guess(board, %Coordinate{col: 5, row: 8})
+      {:hit, :square, :no_win, board} = Board.guess(board, %Coordinate{col: 5, row: 9})
+      {:hit, :square, :no_win, board} = Board.guess(board, %Coordinate{col: 6, row: 8})
+
+      # At this point, the only coordinate not guessed is: (6, 9) - Square islannd
+      assert {:hit, :square, :win, board} = Board.guess(board, %Coordinate{col: 6, row: 9})
+    end
+  end
+
+  defp partially_complete_board(_context) do
+    [
+      board:
+        Board.new()
+        |> Map.put(:l_shape, island(:l_shape, 4, 2))
+        |> Map.put(:s_shape, island(:s_shape, 4, 3))
+    ]
+  end
+
+  defp complete_board(_context) do
+    [
+      board:
+        Board.new()
+        |> Board.position_island(:l_shape, island(:l_shape, 1, 1))
+        |> Board.position_island(:s_shape, island(:s_shape, 5, 1))
+        |> Board.position_island(:atoll, island(:atoll, 3, 1))
+        |> Board.position_island(:dot, island(:dot, 7, 2))
+        |> Board.position_island(:square, island(:square, 5, 8))
+    ]
   end
 
   defp island(shape, up_left_col, up_left_row),
